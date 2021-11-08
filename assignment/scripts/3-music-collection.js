2483428743c0e51d0321640a753548a1578d6a37
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-08 17:58:02 -0600

    addToCollection complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        title: `${title}`,
+        artist: `${artist}`,
+        year: `${yearPublished}`
+    }
+    collection.push(album)
+}
+addToCollection('First Born', 'Eyedea & Abilities', '2001')
+console.log(collection);
