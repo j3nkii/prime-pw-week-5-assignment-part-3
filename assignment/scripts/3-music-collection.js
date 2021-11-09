2e521887fbdb0f17683d3fc58db9e5c6f6e1ec82
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-08 18:30:20 -0600

    Function test with 6 cases

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,6 @@
 console.log('***** Music Collection *****')
-
+//Create collection, and function to add to it
 let collection = [];
-
 function addToCollection(title, artist, yearPublished) {
     let album = {
         title: `${title}`,
@@ -9,6 +8,15 @@ function addToCollection(title, artist, yearPublished) {
         year: `${yearPublished}`
     }
     collection.push(album)
+    return album;
 }
-addToCollection('First Born', 'Eyedea & Abilities', '2001')
-console.log(collection);
+
+//test func
+console.log(addToCollection('First Born', 'Eyedea & Abilities', '2001'));
+console.log(addToCollection('Traveller', 'Chris Stapleton', '2015'));
+console.log(addToCollection('Most Known Uknown', 'Three 6 Mafia', '2005'));
+console.log(addToCollection('Black Sands', 'Bonobo', '2010'));
+console.log(addToCollection('Imaginary Appalacia', 'Colter Wall', '2015'));
+console.log(addToCollection('Redneck Shit', 'Wheeler Walker Jr.', '2016'));
+
+console.log('Your current collection is:', collection);
