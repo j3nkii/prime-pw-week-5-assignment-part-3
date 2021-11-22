b9e3a155e55592e20307ec9b3af7b2792202747d
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-22 13:42:53 -0600

    added SOME tracks

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,69 +1,90 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
 //Create collection, and function to add to it
 let collection = [];
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
     let album = {
-        title: `${title}`,
-        artist: `${artist}`,
-        year: `${yearPublished}`
+        title: title,
+        artist: artist,
+        //using to srting to make sure input is always a string and can be accsessed as such
+        year: yearPublished.toString(),
+        tracks: tracks,
     }
-    collection.push(album)
+    collection.push(album);
     return album;
 }
 
-//test func
-console.log(addToCollection('first born', 'eyedea & abilities', '2001'));
-console.log(addToCollection('traveller', 'chris stapleton', '2015'));
-console.log(addToCollection('most known uknown', 'three 6 mafia', '2005'));
-console.log(addToCollection('black sands', 'bonobo', '2010'));
-console.log(addToCollection('imaginary appalacia', 'colter wall', '2015'));
-console.log(addToCollection('redneck shit', 'wheeler walker jr.', '2016'));
 
-console.log('Your current collection is:', collection);
+
+//test func
+console.log('-----Adding-----');
+console.log(addToCollection('First Born', 'Eyedea & Abilities', '2001',[{track: 'One', duration: '2:35'}, {track: 'Music Music', duration: '4:40'}, {track: 'Birth of a Fish', duration: '2:09'},]));
+console.log(addToCollection('Traveller', 'Chris Stapleton', '2015',[{track: 'Traveler', duration: '3:24'}, {track: 'Fire Away', duration: '4:04'}, {track: 'Tennessee Whiskey', duration: '4:53}'},]));
+//console.log(addToCollection('most known uknown', 'three 6 mafia', '2005',[]));
+//console.log(addToCollection('black sands', 'bonobo', '2010',[]));
+//console.log(addToCollection('imaginary appalacia', 'colter wall', '2015',[]));
+//console.log(addToCollection('redneck shit', 'wheeler walker jr.', '2016',[]));
+            //test
+            console.log('Your current collection is:', collection);
+
+
 
 //showCollection function
 console.log('--------show collection---------');
 function showCollection(array) {
-    console.log(array.length);
+    console.log(`You have ${collection.length} albums in Collection`);
     for(album of array) {
-        let values = Object.values(album)
-        console.log(`${values[0]} by ${values[1]} in ${values[2]}`);
+        let values = Object.values(album);
+        console.log(`${values[0]}, by ${values[1]}, published in ${values[2]}`);
     }
 }
-showCollection(collection)//test showCollection
+            //test showCollection
+            showCollection(collection);
 
+
+
+console.log('-----Find by artist-----');
 //create a find artist function
 function findByArtist(artist) {
-    let matchArtist = []
-    artist = artist.toLowerCase()
+    let matchArtist = [];
     for(album in collection) {
-       if(collection[album].artist == artist){
-           matchArtist.push(collection[album])
+        //adding toLowercase to decrease search sensitivity
+       if(collection[album].artist.toLowerCase() === artist.toLowerCase()){
+           matchArtist.push(collection[album]);
        }
     }
     return matchArtist;
 }
-//TESTCASES
-console.log(`Search fo Bonobo:`, findByArtist('Bonobo'));
-console.log(`Search for Skrillex.... :`, findByArtist('Skrillex'));
-//testing to be sure it will add more than one album
-addToCollection('Days to Come', 'Bonobo', '2006')
-console.log(`Search for Bonobo:`, findByArtist('Bonobo'));
+            //TESTCASES
+            console.log(`Search fo Bonobo:`, findByArtist('Bonobo'));
+            console.log(`Search for Skrillex.... :`, findByArtist('Skrillex'));
+            //testing to be sure it will add more than one album
+            //addToCollection('Days to Come', 'Bonobo', '2006',[]);
+            console.log(`Search for Bonobo:`, findByArtist('Bonobo'));
 
-console.log('------STRETCH---------');
 
+
+console.log('------------STRETCH---------------');
 function search(find) {
     let searchResults = [];
-    find = find.toString()
-    find = find.toLowerCase()
     for(album of collection) {
         for(item of Object.values(album)) {
-            if(item.includes(find)) {
+            if(typeof item !== 'object' && item.includes(find)){
                 searchResults.push(album);
+                break;
+            } else if(typeof item === 'object') {
+                for(track of item){
+                    if(Object.values(track).includes(find)){
+                        searchResults.push(album);
+                        break;
+                    }
+                }
             }
+            
         }
     }
     return searchResults;
 }
-console.log(search('CHRIS'));//test
-console.log(search('skrillex'));
+            //test
+            console.log(search('Fire Away'))
+            console.log(search('One'));
+            //console.log(search('skrillex'));
--- readme.md
+++ readme.md
@@ -17,31 +17,31 @@ Update the `3-music-collection.js` file to do the following:
 
 - Create a variable `collection` that starts as an empty array.
 
-- Add a function named `addToCollection`. This function should:
-  - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-  - Create a new object having the above properties
-  - Add the new object to the end of the `collection` array
-  - Return the newly created object
+- xAdd a function named `addToCollection`. This function should:
+  - xTake in the album's `title`, `artist`, `yearPublished` as input parameters
+  - xCreate a new object having the above properties
+  - xAdd the new object to the end of the `collection` array
+  - xReturn the newly created object
 
-- Test the `addToCollection` function:
-  - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-  - Console.log each album as added using the returned value.
-  - After all are added, console.log the `collection` array.
+- xTest the `addToCollection` function:
+  - xAdd 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
+  - xConsole.log each album as added using the returned value.
+  - xAfter all are added, console.log the `collection` array.
 
-- Add a function named `showCollection`. This function should:
-  - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
-  - Console.log the number of items in the array.
-  - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+- xAdd a function named `showCollection`. This function should:
+  - xTake in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+  - xConsole.log the number of items in the array.
+  - xLoop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
 
-- Test the `showCollection` function.
+- xTest the `showCollection` function.
 
-- Add a function named `findByArtist`. This function should:
-  - Take in `artist` (a string) parameter
-  - Create an array to hold any results, empty to start
-  - Loop through the `collection` and add any objects with a matching artist to the array.
-  - Return the array with the matching results. If no results are found, return an empty array.
+- xAdd a function named `findByArtist`. This function should:
+  - xTake in `artist` (a string) parameter
+  - xCreate an array to hold any results, empty to start
+  - xLoop through the `collection` and add any objects with a matching artist to the array.
+  - xReturn the array with the matching results. If no results are found, return an empty array.
 
-- Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
+- xTest the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
 
 > When testing your functions, write all tests in the JavaScript file!
 
